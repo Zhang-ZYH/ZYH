ff1c4d5aa87f261ed4cd061f1aa6b2c5cad0132e
Zhang-ZYH <64271180+Zhang-ZYH@users.noreply.github.com>
2022-08-22 02:48:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ def get_weather():
   res = requests.get(url).json()
   weather = res['data']['list'][0]
   weather_str=str(weather['low'])+"-"+str(weather['high'])
-  return weather['weather'],weather['low']+"-"+weather["high"]
+  return weather_str
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
